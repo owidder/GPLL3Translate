3e232699396d489da7c5779d1af0aafaedef3a56
Oliver Widder <oliver.widder@ext.hlag.com>
2024-11-16 18:34:19 +0100

    Revert "refactor: Replace puppeteer with html-pdf for PDF generation"
    This reverts commit de36ddfe3e208af0746748b039f228bc8129358f.

--- puppeteer/transform_to_pdf.js
+++ puppeteer/transform_to_pdf.js
@@ -1,36 +1,9 @@
-const pdf = require('html-pdf');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
 (async () => {
-  const folderPath = '/Users/oliverwidder/dev/github/GPLL3Translate/tables'; // Replace with your folder path
-  const files = fs.readdirSync(folderPath).filter(file => file.endsWith('.html'));
-
-  files.forEach(file => {
-    const filePath = path.join(folderPath, file);
-    const html = fs.readFileSync(filePath, 'utf8');
-    const outputFilePath = path.join(folderPath, `${path.basename(file, '.html')}.pdf`);
-
-    const options = {
-      format: 'A4',
-      orientation: 'landscape',
-      border: {
-        top: '0.5in',
-        right: '0.5in',
-        bottom: '0.5in',
-        left: '0.5in'
-      },
-      type: 'pdf',
-      quality: '100',
-      renderDelay: 1000,
-      timeout: 30000
-    };
-
-    pdf.create(html, options).toFile(outputFilePath, (err, res) => {
-      if (err) return console.log(err);
-      console.log(res);
-    });
-  });
+  const browser = await puppeteer.launch();
   const folderPath = '/Users/oliverwidder/dev/github/GPLL3Translate/tables'; // Replace with your folder path
   const files = fs.readdirSync(folderPath).filter(file => file.endsWith('.html'));
 
